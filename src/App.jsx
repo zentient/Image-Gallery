2e65de3b342c8820685436c13edecad7af1f6bc3
zentient <67522653+zentient@users.noreply.github.com>
2021-01-14 22:52:37 -0400

    ImageCard Component created.
    - Created the components directory and a separated component for displaying the image with the returned data.
    - Created the image states.
    - Created a fetch method to use the Pixabay API to display images for the gallery.

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,31 @@
-import React, { Component } from 'react'
+import React, { useState, useEffect } from "react";
+import ImageCard from "./components/ImageCard";
 
-export class App extends Component {
-  render() {
-    return (
-      <div>
-        <h1>Welcome!</h1>
-      </div>
+export default function App() {
+  // Images States
+  const [images, setImages] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+  const [searchTerm, setSearchTerm] = useState("");
+
+  useEffect(() => {
+    fetch(
+      `https://pixabay.com/api/?key=${process.env.REACT_APP_PIXABAY_API_KEY}&q=${searchTerm}&image_type=photo&pretty=true`
     )
-  }
-}
+      .then((res) => res.json())
+      .then((data) => {
+        setImages(data.hits);
+        setIsLoading(false);
+      })
+      .catch((err) => console.log(err));
+  }, []);
 
-export default App
+  return (
+    <div className="container mx-auto">
+      <div className="grid grid-cols-4 gap-4">
+        {images.map((image) => {
+          return <ImageCard key={image.id} image={image} />;
+        })}
+      </div>
+    </div>
+  );
+}
